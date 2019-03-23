9105cdef4ada7ba8bff82c0312e01883b35da743
Lucy Wingard <44914634+wingardl@users.noreply.github.com>
2019-03-23 14:29:01 -0400

    test q2

--- tech_together/lease_interpreter/admin.py
+++ tech_together/lease_interpreter/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Question
-
+from .models import Question2
 # Register your models here.
 
 admin.site.register(Question)
+admin.site.register(Question2)
--- tech_together/lease_interpreter/models.py
+++ tech_together/lease_interpreter/models.py
@@ -7,3 +7,8 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+
+class Question2(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
