89dbc8b433c9c11502cdc1ff25e7dfb18b40858e
Lucy Wingard <44914634+wingardl@users.noreply.github.com>
2019-03-23 11:02:41 -0400

    trying to add test model

--- tech_together/tech_together/settings.py
+++ tech_together/tech_together/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tech_together'
+    'tech_together',
+    'lease_interpreter'
 ]
 
 MIDDLEWARE = [
