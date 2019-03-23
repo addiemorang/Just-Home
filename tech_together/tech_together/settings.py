6a54b5843be2a55b1897a64dc02857b1f98b6d1b
Lucy Wingard <44914634+wingardl@users.noreply.github.com>
2019-03-23 12:51:17 -0400

    update allowed hosts

--- tech_together/tech_together/settings.py
+++ tech_together/tech_together/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = '^_vjdkp7q1y_)u1bdgz$-%+zlm_a(0#bv-!d-h4md!yylh%awp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['addiemorang.github.io', '127.0.0.1']
+ALLOWED_HOSTS = [ 
+                 'addiem.pythonanywhere.com', 
+                 'www.addiem.pythonanywhere.com', 
+                 '.pythonanywhere.com'
+                 'addiemorang.github.io',
+                 '127.0.0.1'
+ ]
 
 
 # Application definition
