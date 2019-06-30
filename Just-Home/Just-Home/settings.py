f459384e2ec606337c9f9a83978f3a5293db3bed
addiemorang <addmorang@gmail.com>
2019-06-30 13:10:06 -0400

    fix menu button

--- Just-Home/Just-Home/settings.py
+++ Just-Home/Just-Home/settings.py
@@ -131,7 +131,7 @@ TEMPLATES = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/JHappl/static/'
 
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static')
