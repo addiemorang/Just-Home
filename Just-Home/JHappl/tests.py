1fa464ea2a9c113f9b10f1a50919e87a66970f59
addiemorang <addmorang@gmail.com>
2019-06-30 12:22:56 -0400

    Remove test case
    tests w/ db will create new test db in mongo

--- Just-Home/JHappl/tests.py
+++ Just-Home/JHappl/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from JHappl.models import Author
 # Create your tests here.
 
-class TestAuthor(TestCase):
-
-    def setup(self):
-        sample = Author.objects.create(name='addie', email='sample@sample.com')
-        sample.save()
+# class TestAuthor(TestCase):
+#
+#     def setup(self):
+#         sample = Author.objects.create(name='addie', email='sample@sample.com')
+#         sample.save()
